8815e1b74289a1368eb021c0cc1a8493299c558d
Bryce Campbell <tonyhawk2100@gmail.com>
2020-03-21 12:03:37 -0700

    remove comments.

--- Formula/rext.rb
+++ Formula/rext.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Rext < Formula
   desc "tool that can quickly change the extension of many files as simply as it is to use mv to rename a single file."
   homepage ""
@@ -10,21 +7,10 @@ class Rext < Formula
   depends_on :xcode => ["11.0", :build]
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
     system "make", "install", "prefix=#{prefix}"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test rext`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/rext", "--help"
   end
 end

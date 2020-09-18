101703942e662c8951292d698c092c54d7bd4ff9
Bryce <tonyhawk2100@gmail.com>
2020-09-18 11:17:26 -0700

    update mac repository.

--- Formula/rext.rb
+++ Formula/rext.rb
@@ -1,13 +1,14 @@
 class Rext < Formula
   desc "tool that can quickly change the extension of many files as simply as it is to use mv to rename a single file."
   homepage ""
-  url "https://github.com/bryceac/rext/archive/rext-0.1.tar.gz"
-  sha256 "f5bd09489e397f15d5e0f885c241a79cc90d83805f08afbfca605e29759b921c"
+  url "https://github.com/bryceac/rext/archive/rext-0.2.tar.gz"
+  sha256 "655d2f12d2288e6d9efb4487fbb3283beefc2e0f457d4d65cd936290923c7720"
 
   depends_on :xcode => ["11.0", :build]
 
   def install
     system "make", "install", "prefix=#{prefix}"
+    man1.install "rext.1"
   end
 
   test do

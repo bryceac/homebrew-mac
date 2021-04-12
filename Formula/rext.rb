f8ba291c5d2dbba0898982e2b473d0e501d0d3c0
bryceac <tonyhawk2100@gmail.com>
2021-04-12 10:32:22 -0700

    update hash

--- Formula/rext.rb
+++ Formula/rext.rb
@@ -2,7 +2,7 @@ class Rext < Formula
   desc "tool that can quickly change the extension of many files as simply as it is to use mv to rename a single file."
   homepage ""
   url "https://github.com/bryceac/rext/archive/rext-0.2.tar.gz"
-  sha256 "670f1846bb8b22c593a961dbd5f9ff9b2c81f7c0e4ecab0cdd985f8980848ea11"
+  sha256 "70f1846bb8b22c593a961dbd5f9ff9b2c81f7c0e4ecab0cdd985f8980848ea11"
 
   depends_on :xcode => ["11.4", :build]
 
